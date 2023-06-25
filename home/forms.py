ed8a6c04ba4443f856a0032640806bed89ef4fd8
Rahul Madan <madan.rahul9@gmail.com>
2023-06-25 13:43:17 +0530

    added classifier from github repo and added excel file upload to take only .xlsx files

--- home/forms.py
+++ home/forms.py
@@ -3,6 +3,10 @@ from .models import ExcelFile
 
 
 class ExcelFileUploadForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(ExcelFileUploadForm, self).__init__(*args, **kwargs)
+        self.fields['excel_file'].widget.attrs.update({'accept': '.xlsx'})
+
     class Meta:
         model = ExcelFile
         fields = ('excel_file',)
