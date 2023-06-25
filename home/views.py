25d8a925d6263ebb7bcab6d184bba931815e4ed1
Rahul Madan <madan.rahul9@gmail.com>
2023-06-25 10:25:43 +0530

    added functionality to change uploaded file's name and added todos

--- home/views.py
+++ home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .forms import ExcelFileUploadForm
-
+import os
 
 # Create your views here.
 
@@ -31,8 +31,21 @@ def get_predictions(request):
         if form.is_valid():
             instance = form.save(commit=False)
             instance.save()
-            # Add your logic to process the file here
-            return render(request, 'success.html')
+
+            # Check if the file is a .xlsx file
+            uploaded_file = request.FILES['excel_file']
+            filename, file_extension = os.path.splitext(uploaded_file.name)
+            if file_extension.lower() == '.xlsx':
+                # Rename the file
+                new_filename = 'new_name.xlsx'
+                os.rename(instance.excel_file.path, os.path.join(os.path.dirname(instance.excel_file.path), new_filename))
+
+                # Add your logic to process the file here
+                return render(request, 'success.html')
+            else:
+                # Handle the case when the file is not a .xlsx file
+                instance.delete()
+                # messages.error(request, 'Invalid file format. Please upload a .xlsx file.')
     else:
         form = ExcelFileUploadForm()
     return render(request, 'get_predictions.html', {'form': form})
